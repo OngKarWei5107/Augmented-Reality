14711c356a30ed89752279c0dd693edb8a17d9ed
TanPenThe <83713083+TanPenThe@users.noreply.github.com>
2021-06-10 16:28:42 +0800

    vbutton

--- VbuttonManager.cs
+++ VbuttonManager.cs
@@ -4,30 +4,27 @@ using UnityEngine;
 using Vuforia;
 using UnityEngine.Video;
 
-public class VbuttonManager : MonoBehaviour, IVirtualButtonEventHandler
+public class VButtonManager : MonoBehaviour, IVirtualButtonEventHandler
 {
-    public VideoPlayer player;
+
+    public VideoPlayer videoPlayer;
     public GameObject playButton;
     private int x = 0;
     public AudioSource audioSource;
+
     // Start is called before the first frame update
     void Start()
     {
         GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     public void OnButtonPressed(VirtualButtonBehaviour vb)
     {
         x++;
-        if (x%2 !=0)
+        if (x%2 != 0)
         {
-            player.Play();
+            videoPlayer.Play();
             playButton.GetComponent<Renderer>().enabled = false;
             audioSource.Pause();
         }
@@ -36,12 +33,12 @@ public class VbuttonManager : MonoBehaviour, IVirtualButtonEventHandler
 
     public void OnButtonReleased(VirtualButtonBehaviour vb)
     {
-        x++;
         if (x % 2 == 0)
         {
-            player.Play();
+            videoPlayer.Pause();
             playButton.GetComponent<Renderer>().enabled = true;
             audioSource.Play();
         }
+        
     }
 }
